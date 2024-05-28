fe88ac4dd196a5ac2e2d4beafb51a994c5fd616d
nachool <nachma.olivia1394@gmail.com>
2024-05-28 18:53:10 +0700

    emnambah class abstraksi

--- noPewarisan/noPewarisan.cpp
+++ noPewarisan/noPewarisan.cpp
@@ -8,7 +8,7 @@ public:
     }
 };
 
-class derivedClass : public  {
+class derivedClass : public baseClass {
 public:
     void perkenalan() {
         cout << "Hallo saya Function dari derived class";
