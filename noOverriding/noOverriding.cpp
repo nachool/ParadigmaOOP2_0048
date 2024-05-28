e59b428ed77cba322abf54d0da97ed8139dbe53a
nachool <nachma.olivia1394@gmail.com>
2024-05-28 18:43:12 +0700

    memanggil derived class.a dan perkenalan.a

--- noOverriding/noOverriding.cpp
+++ noOverriding/noOverriding.cpp
@@ -3,20 +3,23 @@ using namespace std;
 
 class baseClass {
 public:
-    virtual void perkenalan() final {
+    virtual void perkenalan(){
         cout << "Hallo saya function dari base class";
     }
 };
 
 class derivedClass : public baseClass {
 public:
-    void perkenalan() {
+    void perkenalan(){
         cout << "Hallo saya Function dari derived class";
     }
 };
 
 int main()
 {
-    
+    derivedClass a;
+    a.perkenalan();
+
+    return 0;
 }
 
